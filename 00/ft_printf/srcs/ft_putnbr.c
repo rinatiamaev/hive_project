14ad7d8f88bd675a7738c50d070af4ce83abbf37
rinat iamaev <rinatiamaev@gmail.com>
2024-10-26 15:42:13 +0300

    putnbr update

--- 00/ft_printf/srcs/ft_putnbr.c
+++ 00/ft_printf/srcs/ft_putnbr.c
@@ -2,13 +2,21 @@
 
 int	ft_putnbr(int n)
 {
-	char *res;
-	int		len;
+	int	len;
 
-	res = ft_itoa(n);
-	if (!res)
-		return (0);
-	len = ft_putstr(res);
-	free(res);
+	len = 0;
+	if (n == -2147483648)
+	{
+		len += write(1, "-2147483648", 11);
+		return (len);
+	}
+	if (n < 0)
+	{
+		len += write(1, "-", 1);
+		n = -n;
+	}
+	if (n >= 10)
+		len += ft_putnbr(n / 10);
+	len += ft_putchar(n % 10 + '0');
 	return (len);
 }
