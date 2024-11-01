a1be0be4ad51b54b551846b2f26e2b15163d4315
rinat iamaev <rinatiamaev@gmail.com>
2024-11-01 16:59:24 +0200

    ft_strnstr

--- 00/libft/ft_strnstr.c
+++ 00/libft/ft_strnstr.c
@@ -34,3 +34,59 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	}
 	return (NULL);
 }
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: riamaev <riamaev@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 17:10:33 by riamaev           #+#    #+#             */
+/*   Updated: 2024/10/29 09:25:59 by riamaev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (*little == '\0')
+		return ((char *)big);
+	i = 0;
+	while (big[i] && i < len)
+	{
+		if (big[i] == little[0])
+		{
+			j = 0;
+			while (little[j] && big[i + j] == little[j] && (i + j) < len)
+				j++;
+			if (little[j] == '\0')
+				return ((char *)&big[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
+/*
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	n;
+
+	if (*little == '\0')
+		return ((char *)big);
+	n = ft_strlen(little);
+	if (len == 0)
+		return (0);
+	while (*big && n <= len)
+	{
+		if (*big == *little && ft_strncmp(big, little, n) == 0)
+			return ((char *)big);
+		big++;
+		len++;
+	}
+	return (NULL);
+}
+*/
