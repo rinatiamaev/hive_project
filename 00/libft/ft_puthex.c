da091f6b3bb05c9aacc3782ba1e3fe9c24a21d5b
rinat iamaev <rinatiamaev@gmail.com>
2024-11-23 11:18:29 +0200

    new libft

---
+++ 00/libft/ft_puthex.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_puthex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: riamaev <riamaev@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 10:10:18 by riamaev           #+#    #+#             */
+/*   Updated: 2024/11/06 10:10:20 by riamaev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_puthex(unsigned long long n, const char type)
+{
+	int	len;
+
+	len = 0;
+	if (n >= 16)
+	{
+		len += ft_puthex(n / 16, type);
+		len += ft_puthex(n % 16, type);
+	}
+	else
+	{
+		if (n < 10)
+			len += ft_putchar(n + '0');
+		else
+		{
+			if (type == 'x')
+				len += ft_putchar(n - 10 + 'a');
+			else if (type == 'X')
+				len += ft_putchar(n - 10 + 'A');
+		}
+	}
+	return (len);
+}
--- 00/libft/libft.h
+++ 00/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: riamaev <riamaev@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 12:32:50 by riamaev           #+#    #+#             */
-/*   Updated: 2024/10/29 12:38:26 by riamaev          ###   ########.fr       */
+/*   Updated: 2024/11/23 11:13:32 by riamaev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,15 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdarg.h>
 
+int		ft_printf(const char *format, ...);
+int		ft_putchar(char c);
+int		ft_putstr(char *str);
+int		ft_putnbr(int n);
+int		ft_putpointer(unsigned long long p);
+int		ft_putui(unsigned int n);
+int		ft_puthex(unsigned long long num, const char type);
 int		ft_atoi(char *str);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
